ed44aeaae0d9c9664be8f9c7443f199dc1573f00
andrewvalentovich <andrich_14@mail.ru>
2022-06-19 22:00:05 +0300

    Nested structure of vue components

---
+++ resources/js/components/ArticleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <SingleArticleComponent></SingleArticleComponent>
+    </div>
+</template>
+
+<script>
+    import SingleArticleComponent from "./SingleArticleComponent";
+    export default {
+        name: "ArticleComponent",
+
+        components: {
+            SingleArticleComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/SingleArticleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        SingleArticleComponent
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SingleArticleComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
