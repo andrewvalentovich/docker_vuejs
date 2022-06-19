0c5e8f67b06006b2caf546da151d57a546f4f4aa
andrewvalentovich <andrich_14@mail.ru>
2022-06-19 21:38:04 +0300

    vue project is created

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+3515129498484882013=02184-921742109jd-1d
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
